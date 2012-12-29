2d73813da7ceb242a877a59f1b4d4f07d884fa9d
GamyHuang <hguangm2009@gmail.com>
2012-12-29 17:34:24 +0800

    add commit shell

--- src/com/orange/weiboservice/Award.java
+++ src/com/orange/weiboservice/Award.java
@@ -52,9 +52,9 @@ public class Award {
 	
 	public void sendDailyAwardMessage(String userId, String toUserId, String opusName, int rank) {
 		// 传入的下标从1开始的
-		String message = "您的画作["+opusName+"］荣登今日画榜第"+ rank+"名，获得"+ AwardType.DAILY.coins()[rank-1]+"金币。" +
+		String message = "你的画作["+opusName+"］荣登今日画榜第"+ rank+"名，获得"+ AwardType.DAILY.coins()[rank-1]+"金币。" +
 				"期望再接再厉，也欢迎加入猜猜画画玩家QQ群[228119679]和玩家们一起交流, 关注新浪微博[@猜猜画画手机版]或" +
-				"腾讯微博[猜猜画画手机版]获取每天新鲜画榜!";
+				"腾讯微博[@drawlively]获取每天新鲜画榜!";
 		
 		MessageManager.creatMessage(dbClient, Message.MessageTypeText, userId, toUserId, 
 				null, message, 0.0, 0.0, null, 0);
@@ -62,9 +62,9 @@ public class Award {
 	
 	public void sendContestAwardMessage(String userId, String toUserId, String contestSubject, int rank, int participatorCount) {
 		// 传入的下标从1开始的
-		String message = "恭喜您在参加#"+contestSubject+"#的"+ participatorCount +"名玩家中脱颖而出，荣获第"+ rank+"名！ 获得"+AwardType.CONTEST.coins()[rank-1]+"金币。" +
+		String message = "恭喜你在参加#"+contestSubject+"#的"+ participatorCount +"名玩家中脱颖而出，荣获第"+ rank+"名！ 获得"+AwardType.CONTEST.coins()[rank-1]+"金币。" +
 				"期望再接再厉，也欢迎加入猜猜画画玩家QQ群[228119679]和玩家们一起交流, 关注新浪微博[@猜猜画画手机版]或" +
-				"腾讯微博[猜猜画画手机版]获取每天新鲜画榜!";
+				"腾讯微博[@drawlively]获取每天新鲜画榜!";
 		
 		MessageManager.creatMessage(dbClient, Message.MessageTypeText, userId, toUserId, 
 				null, message, 0.0, 0.0, null, 0);
